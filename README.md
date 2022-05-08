80dbf322d3b3796de26bbe0929875d5b516026c0
Ryan Saldaen <ryan.saldaen@gmail.com>
2022-05-08 11:47:16 -0700

    password generates, started read.ME, figured early error with mislabeled propertys (C in lowercase and uppercase were found in some code preventing code from working properly

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# JavaScript-Password-Generator
+# Password Vomit
+
+## User Story
+```
+AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security
+```
+
+## Description
+```
+The goal of the site is to RANDOMLY generate a password between 8 and 128 characters using at least one character types from Uppercase, Lowercase, Numbers, and special Characters.  A prompt will ask which ones you would like your password to include and if you chose at least one character type it will generate a random password from them.  If not it will prompt you to choose atleast one type.  Once the criteria is filled it will vomit up a password of your chosen length randomly chosen from the types you choose.
+```
+
+## Making the PASSWORD
+![image](https://user-images.githubusercontent.com/101837927/167310728-bcb28590-ebaa-4f78-bfba-7ef6ba2d4275.png)
+
+## PASSWORD VOMITTED!!!
+![image](https://user-images.githubusercontent.com/101837927/167310763-e929b178-158a-42cb-9c02-f94bc1f22d8a.png)
--- script.js
+++ script.js
@@ -1,11 +1,78 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
-  var password = "password";
-  // TODO: add code to generate the password here
+var uppercase = [ 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','Y','Z'];
+var lowercase = [ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z'];
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var special = [ '@', '%', '+', '/', "'",'!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.', ];
 
-  return password;
+
+
+  // TODO: add code to generate the password here
+// password must be between 8 - 128 characters
+// must include lowercase, uppercase, numeric, and/or special characters
+// each character type is used atleast once
+function questions() {
+  var isValid = false;
+  do {
+    var length = prompt("Choose password length between 8 and 128 characters");
+    var askNumbers = confirm("Do you want your password to include numbers?");
+    var askLowerCase = confirm("Do you want your password to include lower case letters?");
+    var askUpperCase = confirm("Do you want your password to include upper case letters?");
+    var askSpecial = confirm("Do you want your password to include special characters?");
+    var responses = {
+      length: length,
+      askNumbers: askNumbers,
+      askLowerCase: askLowerCase,
+      askUpperCase: askUpperCase,
+      askSpecial: askSpecial
+    } 
+    if((length < 8)||(length > 128))
+    alert("Choose number between 8 and 128");
+    else if((!askNumbers)&&(!askLowerCase)&&(!askUpperCase)&&(!askSpecial))
+    alert("Must choose at least one type.");
+    else
+    isValid = true;
+
+  } while(!isValid);
+  return responses;
+}
+
+function generatePassword() {
+  var passwordOptions = questions();
+  var possibleCombo = [];
+  var finalPassword = "";
+
+
+
+  if (passwordOptions.askNumbers) {
+    for (var i of numbers)
+      possibleCombo.push(i);
+  }
+  if (passwordOptions.askLowerCase) {
+    for (var i of lowercase)
+      possibleCombo.push(i);
+  }
+  if (passwordOptions.askUpperCase) {
+    for (var i of uppercase)
+      possibleCombo.push(i);
+  }
+  if (passwordOptions.askSpecial) {
+    for (var i of special)
+      possibleCombo.push(i);
+  }
+
+
+  console.log(possibleCombo);
+
+
+  for (var i = 0; i < passwordOptions.length; i++) {
+    finalPassword += possibleCombo[Math.floor(Math.random() * possibleCombo.length)];
+    
+  }
+  console.log(finalPassword);
+
+  return finalPassword;
 }
 
 // Write password to the #password input
@@ -14,8 +81,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
